f3965eea9ba796034be757ca4cdc1f39650e3352
Usaid ok <usaidokdiv@gmail.com>
2023-11-25 19:06:31 +0530

    Project started

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Todo.css'
+function Todo() {
+    return (
+        <div>
+            <div className="todo-app">
+                <div className="container">
+                    <div className="row">
+                        <div className="outer-box">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+export default Todo;

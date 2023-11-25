493b66ccf30cc6aaf1fea7c84c2a6d86362fe48f
Usaid ok <usaidokdiv@gmail.com>
2023-11-25 20:24:02 +0530

    Desing Just Completed

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -3,12 +3,17 @@ import './Todo.css'
 function Todo() {
     return (
         <div>
-            <div className="todo-app">
-                <div className="container">
-                    <div className="row">
-                        <div className="outer-box">
-                            
-                        </div>
+            <div className="container">
+                <div className="row">
+                    {/* ----Heading --- */}
+                    <div className="outer-box">
+                        <h1 className="heading">Todo List</h1>
+                        <h3 className="paragraph">Whoop, it's Wednesday</h3>
+                    </div>
+                    {/* ---------Input Box -------- */}
+                    <div className="input-box">
+                        <input type="text" placeholder='Add item..' />
+                        <button>Add</button>
                     </div>
                 </div>
             </div>
